ce696cd9c7dc07935f9d3ffaeb3cf1446a1fe2f9
asif <asif@mail.com>
2022-03-07 16:57:50 +0530

    upload functionality added

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import json
 import time
 import boto3
-
+from werkzeug.utils import secure_filename
 from flask import Flask, jsonify, render_template, request, session
+
 app = Flask(__name__)
 app.secret_key = "hello"
 token = None
+session_token = ""
+secret_key = ""
+access_key= ""
 @app.route('/')
 def index():
     return render_template('./index.html')
@@ -36,13 +40,28 @@ def accesskey():
 def buck():
     token = session.get('token')
     response,keys = getbuckets(token)
-    file1=response[0]
-    file2=response[1]
-    file3=response[2]
-    file4=response[3]
-    file5=response[4]
-    return render_template('./files.html',  file1 = file1,file2 = file2,file3 = file3,file4 = file4,file5 = file5 )
+    # file1=response[0]
+    # file2=response[1]
+    # file3=response[2]
+    # file4=response[3]
+    # file5=response[4]
+    total = []
+    for files in response:
+       
+        total.append(files)
+    
+    # file1 = file1,file2 = file2,file3 = file3,file4 = file4,file5 = file5
+    return render_template('./files.html', files=total )
 
+@app.route('/upload')
+def home():
+    return render_template("upload.html")
+
+@app.route('/uploading', methods=['post'])
+def upload():
+    uploading()
+    msg = "Upload Done ! You can check by going to /files !!!"
+    return render_template("uploaded.html", msg =msg)
 
 @app.route('/loggedout')
 def loggedout():
@@ -62,12 +81,17 @@ def getbuckets(token):
     credentials = client.get_credentials_for_identity(IdentityId=identityId, Logins ={
         "cognito-idp.ap-south-1.amazonaws.com/ap-south-1_leXbHfpyb":token
     }) 
+
+    global session_token,secret_key,access_key
+
     access_key = credentials['Credentials']['AccessKeyId']
     secret_key = credentials['Credentials']['SecretKey']
     session_token = credentials['Credentials']['SessionToken']
     identity_id = credentials['IdentityId']
 
-    keys = [access_key,secret_key,session_token,identity_id]
+    #global keys
+
+    keys = [ access_key , secret_key , session_token ]
 
     session = boto3.Session( aws_access_key_id=access_key, aws_secret_access_key=secret_key, aws_session_token=session_token, region_name='ap-south-1')
 
@@ -82,5 +106,27 @@ def getbuckets(token):
         buckets.append(objects.key)
     #print("!!! 🎇✨🎇 Congratulations We Did It 🎇✨🎇 !!!" )
     return buckets,keys
+
+
+def uploading():
+
+   
+    
+    s3 = boto3.client('s3',
+                    aws_access_key_id=access_key,
+                    aws_secret_access_key=secret_key,
+                    aws_session_token=session_token
+                    )
+
+    if request.method == 'POST':
+        img = request.files['file']
+        if img:
+                filename = secure_filename(img.filename)
+                img.save(filename)
+                s3.upload_file(
+                    Bucket = "testingforcognito",
+                    Filename=filename,
+                    Key = filename
+                )
 
 app.run()
